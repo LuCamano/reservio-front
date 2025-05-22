84b5e01c67460c201b424e89bc5dc2ba84da6bd2
SeSMP <se.santamaria@duocuc.cl>
2025-05-22 19:34:05 -0400

    comp

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ComponentsModule } from '../../components/components.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
@@ -11,7 +11,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    ComponentsModule
+]
 })
 export class HomeModule { }
