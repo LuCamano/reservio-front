46d6f61bb2ead4304f4557b520062b79b4c58755
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-06-07 18:26:09 -0400

    versión preliminar , permite ver los detalles de los loscales

--- src/app/pages/locales/locales-routing.module.ts
+++ src/app/pages/locales/locales-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocalesComponent } from './locales.component';
+import { LocalComponent } from './local/local.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LocalesComponent
+  },
+  {
+    path: ':id',
+    component: LocalComponent
   }
-  
-
 ];
 
 @NgModule({
--- src/app/pages/locales/locales.module.ts
+++ src/app/pages/locales/locales.module.ts
@@ -5,12 +5,14 @@ import { LocalesRoutingModule } from './locales-routing.module';
 import { LocalesComponent } from './locales.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LocalComponent } from './local/local.component';
 
 
 
 @NgModule({
   declarations: [
-    LocalesComponent
+    LocalesComponent,
+    LocalComponent
   ],
   imports: [
     CommonModule,
