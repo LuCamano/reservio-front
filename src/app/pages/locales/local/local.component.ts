32c93b3b7ddc5619c689647c8b57a30d6b315053
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-06-10 19:57:50 -0400

    seccion comentarios

--- src/app/pages/locales/local/local.component.ts
+++ src/app/pages/locales/local/local.component.ts
@@ -18,7 +18,7 @@ export class LocalComponent {
     telefono: '(123) 456-7890',
     correo:'juan@gmail.com',
     descripcion: 'Un acogedor café ubicado en el corazón de la ciudad, ideal para disfrutar de un ambiente tranquilo y cálido. Ofrecemos una cuidada selección de cafés de especialidad, tés artesanales y una deliciosa variedad de postres caseros, preparados diariamente con ingredientes frescos. Nuestro espacio cuenta con cómodas mesas, conexión Wi-Fi gratuita y una pequeña biblioteca para quienes deseen relajarse leyendo o trabajar en un entorno inspirador. Además, organizamos eventos culturales como lecturas, música en vivo y talleres los fines de semana. Perfecto para reuniones informales, citas o simplemente para disfrutar de un buen momento con una taza caliente entre manos.',
-    imagenUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Backrooms_model.jpg/1200px-Backrooms_model.jpg',
+    imagenUrl: 'https://cdn0.matrimonios.cl/vendor/7446/3_2/960/jpg/foto-3_8_107446.jpeg',
     imagenMap: 'https://lh3.googleusercontent.com/YOXD2PhKTGyasUdmsGwH-9md1HjSBuQ_6s-6e46IfXGIrearmeqgfWDfleEq-JN9ld8TCISJaffrusemhMw7U8sB-EP0xxqkn8lp=rw-e365-w1375'
   };
 }
