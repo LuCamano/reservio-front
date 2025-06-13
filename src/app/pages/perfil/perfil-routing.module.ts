339daaabb89953144c1bceefdd9403883b4c131f
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-06-13 01:36:48 -0400

    addPropiedad

--- src/app/pages/perfil/perfil-routing.module.ts
+++ src/app/pages/perfil/perfil-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './perfil.component';
+import { VerPropiedadComponent } from './ver-propiedad/ver-propiedad.component';
+import { AddPropiedadComponent } from './add-propiedad/add-propiedad.component';
 
 const routes: Routes = [
   {
     path: '', component: PerfilComponent
+  },
+  {
+    path: 'ver-propiedad', component: VerPropiedadComponent
+  },
+  {
+    path: 'add-propiedad', component: AddPropiedadComponent
   }
 
 ];
---
+++ src/app/pages/perfil/ver-propiedad/ver-propiedad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-propiedad',
+  standalone: false,
+  templateUrl: './ver-propiedad.component.html',
+  styleUrl: './ver-propiedad.component.scss'
+})
+export class VerPropiedadComponent {
+
+}
