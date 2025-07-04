3ad7a60314d5c71aa67fdcf62140a97d9d2a049d
Luciano <lu.camano@duocuc.cl>
2025-07-04 03:18:26 -0400

    fix: ajustar la configuración de HttpClient para incluir interceptores en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
@@ -16,12 +16,12 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
     ComponentsModule
   ],
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
     },
-    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
