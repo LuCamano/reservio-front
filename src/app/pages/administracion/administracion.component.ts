385a71517d79aeb3a0f6f8a3fdc80967728c5fe9
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-07-09 04:04:26 -0400

    malditasea por que no saca los datos

--- src/app/pages/administracion/administracion.component.ts
+++ src/app/pages/administracion/administracion.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AuthService } from '../../services/auth.service';
+import { Usuario } from '../../models/models.interface';
 
 @Component({
   selector: 'app-administracion',
@@ -7,10 +9,18 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   templateUrl: './administracion.component.html',
   styleUrl: './administracion.component.scss'
 })
-export class AdministracionComponent {
+export class AdministracionComponent implements OnInit{
+  private authSvc = inject(AuthService)
   mobileMenuOpen = false;
+  usuario: Usuario | null = null;
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  ngOnInit(){
+    this.authSvc.getCurrentUser().subscribe(user => {
+      this.usuario = user;
+    });
   }
 }
